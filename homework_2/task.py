2be5fd5bc827c31afd0ddaab3b9c6d5e5c9bd1a7
Vladimir <vovazakharov2@gmail.com>
2021-10-20 22:01:11 +0300

    task2

--- homework_2/task.py
+++ homework_2/task.py
@@ -1,14 +1,15 @@
 from bs4 import BeautifulSoup as bs
 import requests
-import pandas as pd
+import views
 
 headers = {'User-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
                     'Authorization':'Basic cG9zdG1hbjpwYXNzd29yZA=='}
 req = requests.get('https://tula.hh.ru/search/vacancy?text=python+developer', headers=headers).text
 soup = bs(req, 'html.parser')
 vacancy = soup.select(".vacancy-serp-item__row_header")
-vacancy_dict = {}
+vacancy_list = []
 for i, pos in enumerate(vacancy):
+    vacancy_dict = {}
     vacancy_name = pos.find(attrs={'data-qa': 'vacancy-serp__vacancy-title'})
     vacancy_name_txt = vacancy_name.text
     vacancy_comp = pos.find(attrs={'data-qa': "vacancy-serp__vacancy-compensation"})
@@ -18,6 +19,10 @@ for i, pos in enumerate(vacancy):
         vacancy_comp_txt = 'не указано'
     vacancy_href = pos.find(attrs={'data-qa': 'vacancy-serp__vacancy-title'})
     vacancy_href_attr = vacancy_href.attrs['href']
-    vacancy_dict[i+1] = [vacancy_name_txt, vacancy_comp_txt, vacancy_href_attr]
-print(vacancy_dict)
-frame = pd.DataFrame(vacancy_dict)
+    vacancy_dict['vacancy name'] = vacancy_name_txt
+    vacancy_dict['salary'] = vacancy_comp_txt
+    vacancy_dict['vacancy link'] = vacancy_href_attr
+    vacancy_list.append(vacancy_dict)
+
+# views.add_vacancies(vacancy_list)
+comp_vacancies = views.search_vacancies()
