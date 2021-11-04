cebfcb4fa31c08a349fb8fd3568da0a5c2d72691
Vladimir <vovazakharov2@gmail.com>
2021-11-04 21:35:48 +0300

    task2 done

--- task1_yahoo.py
+++ task1_yahoo.py
@@ -1,10 +1,7 @@
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from pymongo import MongoClient
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.support.ui import WebDriverWait
 
 
 client = MongoClient('localhost', 27017)
